85f1d34326a3c4ba8f952ffa0a00ff0bcb7d3052
Najmul hoque khan <128982967+najmulgithub@users.noreply.github.com>
2023-03-26 20:57:25 +0530

    used pyCharm editor
    Python String index slicing  practice used by pyCharm editor

---
+++ stringindslic.py
@@ -0,0 +1,6 @@
+i = "Welcome to Najmul"
+print(len(i)) #total character of line(17 with space count.)
+print(i[5]) #get just output char. index no. 5 (m)
+print(i[3:]) #get output char. start from index no.3 to last.
+print(i[2:15:2]) #get output char. start from index no.2 to 15,with added +1 char.
+print(i[-4:]) #here char. counting start from right to left side.
